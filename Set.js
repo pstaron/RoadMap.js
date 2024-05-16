2e61af370e998e2e6a245ec929f0514ad83f2cbd
MrZakaz <adam.staron@vp.pl>
2024-05-16 20:47:05 +0200

    Strings 1.0.1 - new Set

---
+++ Set.js
@@ -0,0 +1,10 @@
+const a = new Set([1, 2, 3, 3 ,2, 7])
+// const b = new Map([
+//     [1, "one"],
+//     [2, "two"],
+//     [4, "four"],
+// ]);
+// console.log(a.add(...b)); // Set(4) {1, 2, 3, 4}
+console.log(a)
+
+console.log(typeof(NaN))
